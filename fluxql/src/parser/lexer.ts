64e7ace0a90a1cc81e9721352d319df3168a283d
Ludovic Aggaï <sepogonangabang@gmail.com>
2025-10-31 09:34:31 +0100

    Update lexer.ts

---
+++ fluxql/src/parser/lexer.ts
@@ -0,0 +1,21 @@
+import { Lexer } from 'chevrotain';
+import * as tokens from './tokens.js';
+
+const allTokens = [
+  tokens.WhiteSpace,
+  tokens.Identifier,
+  tokens.StringLiteral,
+  tokens.NumberLiteral,
+  tokens.Dot,
+  tokens.LParen,
+  tokens.RParen,
+  tokens.Equals,
+  tokens.Greater,
+  tokens.Less,
+  tokens.And,
+  tokens.Or,
+  tokens.Not,
+  tokens.Comma
+];
+
+export const FluxQLLexer = new Lexer(allTokens);
